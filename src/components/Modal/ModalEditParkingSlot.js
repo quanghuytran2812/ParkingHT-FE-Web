4d9fa255016a65709634dd7791150f85e57d432f
quanghuytran2812 <quanghuytran2812@gmail.com>
2023-12-11 13:28:05 +0700

    v12.11.1

--- src/components/Modal/ModalEditParkingSlot.js
+++ src/components/Modal/ModalEditParkingSlot.js
@@ -1,23 +1,19 @@
 import { apiEditParkingSlot } from "apis";
 import "assets/css/modalCommon.css";
 import Loader from "components/Loader";
-import InputField from "components/inputs/InputField";
 import Select from "components/inputs/Select";
 import { memo, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { statusData } from "ultils/contants";
-import { validate } from "ultils/helpers";
 import icons from "ultils/icons";
 
 const ModalEditParkingSlot = ({ open, onClose, handleUpdateTable, dataParkingSlotEdit }) => {
     const { CloseIcon } = icons;
-    const [invalidFields, setInvalidFields] = useState([]);
     const [isloading, setIsloading] = useState(false)
     const [status, setStatus] = useState(dataParkingSlotEdit.delFlag);
     const [parkingSlot, setParkingSlot] = useState({
         pricePerHour: "",
     });
-
     const handleStatusChange = (value) => {
         setStatus(value);
     };
@@ -32,41 +28,38 @@ const ModalEditParkingSlot = ({ open, onClose, handleUpdateTable, dataParkingSlo
 
     const handleEditParkingSlot = async (e) => {
         e.preventDefault();
-        const invalids = validate(parkingSlot, setInvalidFields)
-        if (invalids === 0) {
-            try {
-                const updateParkingS = {
-                    parkingSlotId: dataParkingSlotEdit.parkingSlotId,
-                    area: dataParkingSlotEdit.area,
-                    name: dataParkingSlotEdit.name,
-                    pricePerHour: parkingSlot.pricePerHour || dataParkingSlotEdit.pricePerHour,
-                    vehicleCategory: dataParkingSlotEdit.categoryName,
-                    delFlag: status || dataParkingSlotEdit.delFlag
-                }
-                setIsloading(true)
-                const res = await apiEditParkingSlot(updateParkingS);
-
-                if (res?.statusCode === 200) {
-                    onClose();
-                    handleUpdateTable();
-                    toast.success(`Chỗ đậu xe được cập nhật thành công!`);
-                    setIsloading(false)
-                }
-            } catch (err) {
-                // Handle error
-                if (!err?.response) {
-                    toast.error('Không có phản hồi của máy chủ');
-                } else if (err.response?.status === 400) {
-                    toast.error(`${err.response?.data.detail}`)
-                } else if (err.response?.status === 401) {
-                    toast.error('Không được phép!');
-                } else {
-                    toast.error("Chỗ đậu xe được cập nhật thất bại!")
-                }
-                console.clear();
+        try {
+            const updateParkingS = {
+                parkingSlotId: dataParkingSlotEdit.parkingSlotId,
+                area: dataParkingSlotEdit.area,
+                name: dataParkingSlotEdit.name,
+                pricePerHour: parkingSlot.pricePerHour || dataParkingSlotEdit.pricePerHour,
+                vehicleCategory: dataParkingSlotEdit.categoryName,
+                delFlag: status || dataParkingSlotEdit.delFlag
             }
-            setIsloading(false)
+            setIsloading(true)
+            const res = await apiEditParkingSlot(updateParkingS);
+
+            if (res?.statusCode === 200) {
+                onClose();
+                handleUpdateTable();
+                toast.success(`Chỗ đậu xe được cập nhật thành công!`);
+                setIsloading(false)
+            }
+        } catch (err) {
+            // Handle error
+            if (!err?.response) {
+                toast.error('Không có phản hồi của máy chủ');
+            } else if (err.response?.status === 400) {
+                toast.error(`${err.response?.data.detail}`)
+            } else if (err.response?.status === 401) {
+                toast.error('Không được phép!');
+            } else {
+                toast.error("Chỗ đậu xe được cập nhật thất bại!")
+            }
+            console.clear();
         }
+        setIsloading(false)
 
     };
     if (!open) return null;
@@ -89,17 +82,17 @@ const ModalEditParkingSlot = ({ open, onClose, handleUpdateTable, dataParkingSlo
 
                             {
                                 dataParkingSlotEdit.delFlag === false ? (
-                                    <InputField
-                                        type='number'
-                                        nameKey='pricePerHour'
-                                        className='inputGroup'
-                                        classNameInput='resetpasswordinput'
-                                        value={parkingSlot.pricePerHour}
-                                        onChange={(e) => setParkingSlot(prev => ({ ...prev, pricePerHour: e.target.value }))}
-                                        placeholder="Price Per Hour //Ex: 14000"
-                                        invalidFields={invalidFields}
-                                        setInvalidFields={setInvalidFields}
-                                    />
+                                    <div style={{ marginBottom: '20px' }}>
+                                        <div className="inputGroup">
+                                            <input
+                                                type="number"
+                                                className="resetpasswordinput"
+                                                value={parkingSlot.pricePerHour}
+                                                onChange={(e) => setParkingSlot(prev => ({ ...prev, pricePerHour: e.target.value }))}
+                                                placeholder="Price Per Hour //Ex: 14000"
+                                            />
+                                        </div>
+                                    </div>
                                 ) : (
                                     <Select
                                         itemValue={dataParkingSlotEdit.delFlag}
